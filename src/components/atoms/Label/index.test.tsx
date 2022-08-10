80b3267cf036a5a2adeee14e392d8bc874ccb6d5
k-sone <k-sone@crestec.co.jp>
2022-08-11 07:12:52 +0900

    remove @testing-library/react-hooks
    use renderHook() in
    @testing-library/react instead of @testing-library/react-hooks

--- src/components/atoms/Label/index.test.tsx
+++ src/components/atoms/Label/index.test.tsx
@@ -1,9 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { render, renderHook, screen } from "@testing-library/react";
 import { Label } from ".";
+
+function useLoggedInUser() {
+  return { name: "Alice" };
+}
 
 describe("Label", () => {
   it("foo", () => {
     render(<Label name="foo" />);
     expect(screen.getByText("foo")).toBeTruthy();
+  });
+
+  it("returns logged in user", () => {
+    const { result } = renderHook(() => useLoggedInUser());
+    expect(result.current).toEqual({ name: "Alice" });
   });
 });
